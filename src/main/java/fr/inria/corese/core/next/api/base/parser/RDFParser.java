a24d3da7b80786726217318859149f3799c66a2c
Pierre Maillot <maillpierre@gmail.com>
2025-06-04 17:02:52 +0200

    RDF formats

---
+++ src/main/java/fr/inria/corese/core/next/api/base/parser/RDFParser.java
@@ -0,0 +1,9 @@
+package main.java.fr.inria.corese.core.next.api.base.parser;
+
+public interface RDFParser {
+
+    /**
+     * Gets the RDF format that this parser can parse.
+     */
+    RDFFormat getRDFFormat();
+}
