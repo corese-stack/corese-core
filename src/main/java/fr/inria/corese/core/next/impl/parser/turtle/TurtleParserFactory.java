4012ee7dad39e8fdfd866d7518b9794af61cc126
pierrerene <pierre-rene@probabl.ai>
2025-06-20 13:43:56 +0200

    TurtleParser and turtle g4

---
+++ src/main/java/fr/inria/corese/core/next/impl/parser/turtle/TurtleParserFactory.java
@@ -0,0 +1,18 @@
+package fr.inria.corese.core.next.impl.parser.turtle;
+
+import fr.inria.corese.core.next.api.Model;
+import fr.inria.corese.core.next.api.base.parser.RDFFormat;
+import fr.inria.corese.core.next.api.base.parser.RDFFormats;
+import fr.inria.corese.core.next.api.base.parser.RDFParser;
+import fr.inria.corese.core.next.api.base.parser.RDFParserFactory;
+
+public class TurtleParserFactory implements RDFParserFactory {
+
+    @Override
+    public RDFParser createRDFParser(RDFFormat format, Model model) {
+        if (!format.equals(RDFFormats.TURTLE)) {
+            throw new IllegalArgumentException("Unsupported format : " + format);
+        }
+        return new TurtleParser(model);
+    }
+}
