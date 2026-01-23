465a68cca86aa4797a89a35030b1eb88a79d07cf
Pierre Maillot <maillpierre@gmail.com>
2026-01-23 16:18:05 +0100

    CSV options

--- src/main/java/fr/inria/corese/core/next/impl/sparql/io/serializer/common/CharacterSeparatedValuesSerializer.java
+++ src/main/java/fr/inria/corese/core/next/impl/sparql/io/serializer/common/CharacterSeparatedValuesSerializer.java
@@ -1,4 +1,27 @@
 package fr.inria.corese.core.next.impl.sparql.io.serializer.common;
 
-public class CharacterSeparatedValuesSerializer {
+import fr.inria.corese.core.kgram.core.Mappings;
+import fr.inria.corese.core.next.api.io.IOOptions;
+import fr.inria.corese.core.next.api.sparql.io.serializer.ResultSerializer;
+import fr.inria.corese.core.next.impl.exception.SerializationException;
+
+import java.io.Writer;
+
+public abstract class CharacterSeparatedValuesSerializer implements ResultSerializer {
+
+    private char separator;
+    private Mappings results;
+    private IOOptions config;
+
+    protected CharacterSeparatedValuesSerializer(char separator, Mappings results, IOOptions options) {
+        this.separator = separator;
+        this.results = results;
+        this.config = options;
+    }
+
+    @Override
+    public void write(Writer writer) throws SerializationException {
+
+    }
+
 }
