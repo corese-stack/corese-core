23b1e89e80e00af1f466a69660170fc73658d160
Pierre Maillot <maillpierre@gmail.com>
2026-01-28 10:15:29 +0100

    reorg

--- src/main/java/fr/inria/corese/core/next/api/query/io/serializer/ResultSerializer.java
+++ src/main/java/fr/inria/corese/core/next/api/query/io/serializer/ResultSerializer.java
@@ -1,4 +1,4 @@
-package fr.inria.corese.core.next.api.sparql.io.serializer;
+package fr.inria.corese.core.next.api.query.io.serializer;
 
 import fr.inria.corese.core.next.api.io.serializer.Serializer;
 
