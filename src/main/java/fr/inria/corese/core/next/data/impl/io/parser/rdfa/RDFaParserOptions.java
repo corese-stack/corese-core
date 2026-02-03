86234c90c3b0e480aedd1472327eddcbe8772909
Pierre Maillot <maillpierre@gmail.com>
2026-02-03 11:49:29 +0100

    Minor fix

--- src/main/java/fr/inria/corese/core/next/data/impl/io/parser/rdfa/RDFaParserOptions.java
+++ src/main/java/fr/inria/corese/core/next/data/impl/io/parser/rdfa/RDFaParserOptions.java
@@ -4,6 +4,7 @@ import fr.inria.corese.core.next.data.api.base.io.AbstractIOOptions;
 import fr.inria.corese.core.next.data.io.common.BaseIRIOptions;
 import fr.inria.corese.core.next.data.impl.io.common.IOConstants;
 
+import javax.xml.parsers.SAXParserFactory;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +26,10 @@ public class RDFaParserOptions extends AbstractIOOptions implements BaseIRIOptio
         return this.baseIRI;
     }
 
+    /**
+     *
+     * @return a Map of URI/boolean features values that will be used to configure the SAXParser
+     */
     public Map<String, Boolean> getSAXFeatures() {
         return this.builder.saxFeatures;
     }
@@ -32,7 +37,7 @@ public class RDFaParserOptions extends AbstractIOOptions implements BaseIRIOptio
     public static class Builder extends AbstractIOOptions.Builder<RDFaParserOptions> {
 
         protected String baseIRI = IOConstants.getDefaultBaseURI();
-        protected Map<String, Boolean> saxFeatures;
+        protected Map<String, Boolean> saxFeatures = new HashMap<>();
 
         @Override
         public RDFaParserOptions build() {
@@ -47,10 +52,15 @@ public class RDFaParserOptions extends AbstractIOOptions implements BaseIRIOptio
          */
         public RDFaParserOptions.Builder baseIRI(String baseIRI) {
             this.baseIRI = baseIRI;
-            this.saxFeatures = new HashMap<>();
             return this;
         }
 
+        /**
+         * Set up the options for the {@link SAXParserFactory}. See <a href="http://www.saxproject.org/apidoc/org/xml/sax/package-summary.html#package_description">the list of SAX features</a>.
+         * @param featureuri the SAX feature URI (e.g. "http://xml.org/sax/features/resolve-dtd-uris")
+         * @param value the value desired for the feature
+         * @return this
+         */
         public RDFaParserOptions.Builder setSAXFeature(String featureuri, boolean value) {
             this.saxFeatures.put(featureuri, value);
             return this;
