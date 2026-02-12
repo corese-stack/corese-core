a06aa82e06f0ceb6ac994e682060c57a0488afb3
Pierre Maillot <maillpierre@gmail.com>
2026-02-12 14:13:11 +0100

    quote test fix

--- src/test/java/fr/inria/corese/core/next/query/impl/sparql/io/serializer/common/CharacterSeparatedValuesSerializerTest.java
+++
@@ -1,9 +0,0 @@
-package fr.inria.corese.core.next.query.impl.sparql.io.serializer.common;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CharacterSeparatedValuesSerializerTest {
-
-}
