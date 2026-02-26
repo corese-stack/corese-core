3dc5613bfdb99c9d524694202acb77667b21c17b
pierrerene <pierre-rene@probabl.ai>
2026-02-26 19:51:38 +0100

    added other sparql query

--- src/main/java/fr/inria/corese/core/next/query/sparql/ast/ConstructQueryAst.java
+++ src/main/java/fr/inria/corese/core/next/query/sparql/ast/ConstructQueryAst.java
@@ -5,6 +5,12 @@ import java.util.List;
 /**
  * Abstract Syntax Tree (AST) representation of a SPARQL {@code CONSTRUCT} query.
  * CONSTRUCT { template } WHERE { pattern }.
+ *
+ * PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+ * PREFIX vcard:   <http://www.w3.org/2001/vcard-rdf/3.0#>
+ * CONSTRUCT   { <http://example.org/person#Alice> vcard:FN ?name }
+ * WHERE       { ?x foaf:name ?name }
+ *
  */
 public record ConstructQueryAst(GroupGraphPatternAst constructTemplate, GroupGraphPatternAst whereClause) implements QueryAst {
     public ConstructQueryAst {
