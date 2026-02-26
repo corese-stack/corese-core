065234777345a3b82796985ba3227a0dea9e5adb
pierrerene <pierre-rene@probabl.ai>
2026-02-26 19:51:38 +0100

    added other sparql query

--- src/main/java/fr/inria/corese/core/next/query/sparql/ast/AskQueryAst.java
+++ src/main/java/fr/inria/corese/core/next/query/sparql/ast/AskQueryAst.java
@@ -5,6 +5,11 @@ import java.util.List;
 /**
  * Abstract Syntax Tree (AST) representation of a SPARQL {@code ASK} query.
  * ASK WHERE { pattern } returns a boolean.
+ *
+ * PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+ * ASK  { ?x foaf:name  "Alice" ;
+ *           foaf:mbox  <mailto:alice@work.example> }
+ *
  */
 public record AskQueryAst(GroupGraphPatternAst whereClause) implements QueryAst {
     public AskQueryAst {
--- src/main/java/fr/inria/corese/core/next/query/sparql/ast/DescribeQueryAst.java
+++ src/main/java/fr/inria/corese/core/next/query/sparql/ast/DescribeQueryAst.java
@@ -5,6 +5,13 @@ import java.util.List;
 /**
  * Abstract Syntax Tree (AST) representation of a SPARQL {@code DESCRIBE} query.
  * DESCRIBE (var|uri)* WHERE { pattern } or DESCRIBE (var|uri)*.
+ *
+ * DESCRIBE <http://example.org/>
+ *
+ * PREFIX foaf:   <http://xmlns.com/foaf/0.1/>
+ * DESCRIBE ?x
+ * WHERE    { ?x foaf:mbox <mailto:alice@org> }
+ *
  */
 public record DescribeQueryAst(List<TermAst> described, GroupGraphPatternAst whereClause) implements QueryAst {
     public DescribeQueryAst {
