fe9bccac237f620076d130b88166339bb9d16684
anquetil <nicolas.anquetil@inria.fr>
2026-03-10 17:22:26 +0100

    Cleaning dead code in corese.core.extension

--- src/main/java/fr/inria/corese/core/extension/Extension.java
+++ src/main/java/fr/inria/corese/core/extension/Extension.java
@@ -1,43 +1,25 @@
 package fr.inria.corese.core.extension;
 
 import fr.inria.corese.core.Graph;
-import fr.inria.corese.core.compiler.eval.QuerySolverVisitor;
 import fr.inria.corese.core.kgram.api.core.Edge;
 import fr.inria.corese.core.kgram.api.core.Node;
 import fr.inria.corese.core.kgram.api.query.ProcessVisitor;
 import fr.inria.corese.core.kgram.core.Mapping;
 import fr.inria.corese.core.kgram.core.Mappings;
-import fr.inria.corese.core.kgram.core.ProcessVisitorDefault;
 import fr.inria.corese.core.kgram.core.Query;
-import fr.inria.corese.core.load.LoadException;
-import fr.inria.corese.core.load.Service;
 import fr.inria.corese.core.logic.Distance;
-import fr.inria.corese.core.print.LogManager;
-import fr.inria.corese.core.query.Construct;
-import fr.inria.corese.core.query.ProviderService;
 import fr.inria.corese.core.query.QueryProcess;
-import fr.inria.corese.core.rule.Cleaner;
-import fr.inria.corese.core.rule.RuleEngine;
 import fr.inria.corese.core.sparql.api.IDatatype;
 import fr.inria.corese.core.sparql.api.IDatatypeList;
 import fr.inria.corese.core.sparql.datatype.DatatypeMap;
 import fr.inria.corese.core.sparql.exceptions.EngineException;
-import fr.inria.corese.core.sparql.exceptions.SafetyException;
 import fr.inria.corese.core.sparql.triple.function.term.Binding;
-import fr.inria.corese.core.sparql.triple.function.term.TermEval;
 import fr.inria.corese.core.sparql.triple.parser.ASTQuery;
-import fr.inria.corese.core.sparql.triple.parser.Access;
-import fr.inria.corese.core.sparql.triple.parser.Access.Feature;
 import fr.inria.corese.core.sparql.triple.parser.Access.Level;
 import fr.inria.corese.core.sparql.triple.parser.Context;
 import fr.inria.corese.core.sparql.triple.parser.context.ContextLog;
-import fr.inria.corese.core.visitor.solver.QuerySolverVisitorRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Enumeration;
 
 
 /**
@@ -72,108 +54,12 @@ public class Extension extends Core {
         return getBinding().getLog();
     }
 
-    public IDatatype getContextLog() {
-        if (getLog() == null) {
-            return null;
-        }
-        return DatatypeMap.createObject(getLog());
-    }
-
-    public IDatatype getMessage() {
-        String url = getLog().getMessage();
-        if (url == null) {
-            return null;
-        }
-        String text = new Service().getString(url);
-        if (text == null || text.isEmpty()) {
-            return null;
-        }
-        return DatatypeMap.json(text);
-    }
-
-    public IDatatype parallel(IDatatype name) {
-        return DatatypeMap.newResource(String.format(ProviderService.LOCAL_SERVICE_NS, name.getLabel()));
-    }
-
-
-    // return list of variables bound in environment
-    public IDatatype variables() {
-        ArrayList<IDatatype> list = new ArrayList<>();
-
-        for (Node node : getEnvironment().getQueryNodes()) {
-            if (node != null && node.isVariable() && !node.isBlank()) {
-                Node report = getEnvironment().getNode(node);
-                if (report != null) {
-                    list.add(report.getDatatypeValue());
-                }
-            }
-        }
-        return DatatypeMap.newList(list);
-    }
-
-    /**
-     * Service evaluation report graph recorded in ContextLog
-     */
-    public IDatatype getLogGraph() {
-        if (getLog() == null) {
-            return null;
-        }
-        LogManager man = new LogManager(getLog());
-        try {
-            Graph g = man.parse();
-            return DatatypeMap.createObject(g);
-        } catch (LoadException ex) {
-            logger.error(ex.getMessage());
-            return null;
-        }
-    }
-
-    public IDatatype getLogURL() {
-        if (getLog() == null || getLog().getLink() == null) {
-            return null;
-        }
-        return DatatypeMap.newResource(getLog().getLink());
-    }
-
-    public IDatatype parse(IDatatype dt) throws EngineException {
-        Context c = getCreateContext();
-        if (Access.reject(Feature.LDSCRIPT_SPARQL, c.getLevel())) {
-            throw new SafetyException(TermEval.SPARQL_MESS);
-        }
-        ASTQuery ast = parseQuery(dt.getLabel(), c);
-        if (ast == null) {
-            return null;
-        }
-        return DatatypeMap.createObject(ast);
-    }
-
     ASTQuery parseQuery(String str, Context c) throws EngineException {
         QueryProcess exec = QueryProcess.create();
         Query q = exec.compile(str, c);
         return q.getAST();
     }
 
-
-    public IDatatype list(IDatatype dt) {
-        if (dt.isExtension()) {
-            String datatypeURI = dt.getDatatypeURI();
-            if (IDatatype.MAPPINGS_DATATYPE.equals(datatypeURI)) {
-                return list(dt.getPointerObject().getMappings());
-            } else if (IDatatype.MAPPING_DATATYPE.equals(datatypeURI)) {
-                return list(dt.getPointerObject().getMapping());
-            }
-        }
-        if (dt.isTripleWithEdge()) {
-            return list(dt.getEdge());
-        }
-        if (dt.getNodeObject() != null && dt.getNodeObject() instanceof Enumeration) {
-            return DatatypeMap.newList((Enumeration) dt.getNodeObject());
-        }
-        if (dt.getNodeObject() != null && dt.getNodeObject() instanceof Object[]) {
-            return DatatypeMap.newList((Object[]) dt.getNodeObject());
-        }
-        return DatatypeMap.list();
-    }
 
     IDatatype list(Edge e) {
         IDatatypeList list = DatatypeMap.newList(
@@ -193,16 +79,6 @@ public class Extension extends Core {
     // Mapping as list(list(var, val))
     IDatatype list(Mapping m) {
         return m.getDatatypeList();
-    }
-
-    public IDatatype allEntailment(IDatatype dt) {
-        Construct.setAllEntailment(dt.booleanValue());
-        return dt;
-    }
-
-
-    public IDatatype distance(IDatatype dt1, IDatatype dt2) {
-        return distance(dt1, dt2, null);
     }
 
 
@@ -229,42 +105,8 @@ public class Extension extends Core {
     }
 
 
-    // clean OWL ontology: remove duplicate statements
-    public IDatatype clean() {
-        Cleaner clean = new Cleaner(getGraph());
-        clean.setVisitor(new QuerySolverVisitorRule(new RuleEngine(), getEval()));
-        try {
-            clean.process();
-        } catch (IOException | LoadException | EngineException ex) {
-            logger.error(ex.getMessage());
-        }
-        return DatatypeMap.TRUE;
-    }
-
-
-    // example    
-    IDatatype test(IDatatype dt) {
-        getEnvironment();
-        getProducer();
-        getGraph();
-        getEval();
-        return DatatypeMap.TRUE;
-    }
-
     ProcessVisitor getVisitor() {
         return getEval().getVisitor();
-    }
-
-    public IDatatype closeVisitor() {
-        getEval().setVisitor(new ProcessVisitorDefault());
-        getVisitor().setDefaultValue(DatatypeMap.TRUE);
-        return visitor();
-    }
-
-    public IDatatype openVisitor() {
-        getEval().setVisitor(new QuerySolverVisitor(getEval()));
-        getVisitor().init(getEnvironment().getQuery());
-        return visitor();
     }
 
     /**
@@ -273,22 +115,6 @@ public class Extension extends Core {
      */
     public IDatatype visitor() {
         return cast(getVisitor());
-    }
-
-    public IDatatype query() {
-        return cast(getQuery());
-    }
-
-    public IDatatype ast() {
-        return cast(getAST());
-    }
-
-    public IDatatype edge(IDatatype n) {
-        Edge[] edges = getEnvironment().getEdges();
-        if (edges == null || edges.length < n.intValue()) {
-            return null;
-        }
-        return cast(edges[n.intValue()]);
     }
 
     Query getQuery() {
@@ -312,10 +138,6 @@ public class Extension extends Core {
             default:
                 return fib(n.minus(DatatypeMap.ONE)).plus(fib(n.minus(DatatypeMap.TWO)));
         }
-    }
-
-    public IDatatype fibJava(IDatatype n) {
-        return DatatypeMap.newInstance(fib(n.intValue()));
     }
 
 
