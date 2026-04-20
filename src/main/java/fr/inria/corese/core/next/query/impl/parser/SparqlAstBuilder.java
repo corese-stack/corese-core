e27d710428e6c190eebcedce49ee58c45389ae63
Rémi Cérès <remi.ceres@inria.fr>
2026-04-20 11:37:04 +0200

    #343 [SPARQL 1.1] - Parser and AST : STRLANG

--- src/main/java/fr/inria/corese/core/next/query/impl/parser/SparqlAstBuilder.java
+++ src/main/java/fr/inria/corese/core/next/query/impl/parser/SparqlAstBuilder.java
@@ -872,6 +872,9 @@ public final class SparqlAstBuilder {
             case ASTConstants.FUNCTION_CALL.STRDT -> {
                 return new StrDtAst(args);
             }
+            case ASTConstants.FUNCTION_CALL.STRLANG -> {
+                return new StrLangAst(args);
+            }
             case ASTConstants.FUNCTION_CALL.DATATYPE -> {
                 return new DatatypeAst(args);
             }
@@ -1170,6 +1173,8 @@ public final class SparqlAstBuilder {
                 return this.createConstraint(ASTConstants.FUNCTION_CALL.CONTAINS, args);
             } else if (ctx.STRDT() != null) {
                 return this.createConstraint(ASTConstants.FUNCTION_CALL.STRDT, args);
+            } else if (ctx.STRLANG() != null) {
+                return this.createConstraint(ASTConstants.FUNCTION_CALL.STRLANG, args);
             } else if (ctx.DATATYPE() != null) {
                 return this.createConstraint(ASTConstants.FUNCTION_CALL.DATATYPE, args);
             } else if (ctx.SAME_TERM() != null) {
