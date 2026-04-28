e1e6996729c249aa92a6c4fcbaf30faaf5a5c702
Rémi Cérès <remi.ceres@inria.fr>
2026-04-28 11:49:27 +0200

    refactor: introduce LiteralExpressionAst as common parent for literal type interfaces

--- src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/constraint/XsdDateTimeExpressionAst.java
+++ src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/constraint/XsdDateTimeExpressionAst.java
@@ -1,9 +1,7 @@
 package fr.inria.corese.core.next.query.impl.sparql.ast.constraint;
 
-import fr.inria.corese.core.next.query.impl.sparql.ast.ExprAst;
-
 /**
  * Any expression that can be resolved as an {@code xsd:dateTime} value.
  */
-public interface XsdDateTimeExpressionAst extends ExprAst {
+public interface XsdDateTimeExpressionAst extends LiteralExpressionAst {
 }
--- src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/constraint/XsdDayTimeDurationExpressionAst.java
+++ src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/constraint/XsdDayTimeDurationExpressionAst.java
@@ -1,9 +1,7 @@
 package fr.inria.corese.core.next.query.impl.sparql.ast.constraint;
 
-import fr.inria.corese.core.next.query.impl.sparql.ast.ExprAst;
-
 /**
  * Any expression that can be resolved as an {@code xsd:dayTimeDuration} value.
  */
-public interface XsdDayTimeDurationExpressionAst extends ExprAst {
+public interface XsdDayTimeDurationExpressionAst extends LiteralExpressionAst {
 }
