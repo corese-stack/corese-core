3d349cdb84080dbe518a22a605a47f42dba602a4
anquetil <nicolas.anquetil@inria.fr>
2026-06-09 13:46:03 +0200

    UNRELATED: fixing typo in javadoc comment

--- src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/UpdateRequestUnitAst.java
+++ src/main/java/fr/inria/corese/core/next/query/impl/sparql/ast/UpdateRequestUnitAst.java
@@ -1,7 +1,7 @@
 package fr.inria.corese.core.next.query.impl.sparql.ast;
 
 /**
- * Root interface for all operations related to the SPARQL Update operations listed in <a href="https://www.w3.org/TR/sparql11-update/">SPARQL 1.1 Update</>.
+ * Root interface for all operations related to the SPARQL Update operations listed in <a href="https://www.w3.org/TR/sparql11-update/">SPARQL 1.1 Update</a>.
  */
 public sealed interface UpdateRequestUnitAst permits LoadRequestAst, ClearRequestAst {
 }
