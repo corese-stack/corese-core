c441e7858c301fdd70888374451251193ed56af8
AD\aabdoun <abdessamad.abdoun@inria.fr>
2026-07-22 14:38:48 +0200

    [SPARQL 1.1 Graph Management] - Parser and AST : ADD Query

--- src/test/java/fr/inria/corese/core/next/query/impl/parser/AddUpdateAstBuilderTest.java
+++ src/test/java/fr/inria/corese/core/next/query/impl/parser/AddUpdateAstBuilderTest.java
@@ -1,5 +1,6 @@
 package fr.inria.corese.core.next.query.impl.parser;
 
+import fr.inria.corese.core.next.query.api.exception.QuerySyntaxException;
 import fr.inria.corese.core.next.query.impl.sparql.ast.AddRequestAst;
 import fr.inria.corese.core.next.query.impl.sparql.ast.QueryAst;
 import fr.inria.corese.core.next.query.impl.sparql.ast.UpdateRequestAst;
@@ -9,10 +10,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AddUpdateAstBuilderTest {
+class AddUpdateAstBuilderTest extends AbstractSparqlParserFeatureTest {
 
     private UpdateRequestUnitAst parseUnit(String query) {
-        QueryAst ast = new SparqlParser().parse(query);
+        QueryAst ast = newParserDefault().parse(query);
         UpdateRequestAst update = assertInstanceOf(UpdateRequestAst.class, ast);
         assertEquals(1, update.operations().size(), "one update operation");
         return update.operations().getFirst();
@@ -24,8 +25,8 @@ class AddUpdateAstBuilderTest {
         AddRequestAst add = assertInstanceOf(AddRequestAst.class,
                 parseUnit("ADD GRAPH <http://example.org/s> TO GRAPH <http://example.org/d>"));
         assertFalse(add.silent());
-        assertNotNull(add.source().graph());
-        assertNotNull(add.destination().graph());
+        assertEquals("<http://example.org/s>", add.source().graph().raw());
+        assertEquals("<http://example.org/d>", add.destination().graph().raw());
     }
 
     @Test
@@ -35,7 +36,7 @@ class AddUpdateAstBuilderTest {
                 parseUnit("ADD SILENT DEFAULT TO GRAPH <http://example.org/d>"));
         assertTrue(add.silent());
         assertTrue(add.source().defaultGraph());
-        assertNotNull(add.destination().graph());
+        assertEquals("<http://example.org/d>", add.destination().graph().raw());
     }
 
     @Test
@@ -43,7 +44,74 @@ class AddUpdateAstBuilderTest {
     void addGraphToDefault() {
         AddRequestAst add = assertInstanceOf(AddRequestAst.class,
                 parseUnit("ADD GRAPH <http://example.org/s> TO DEFAULT"));
-        assertNotNull(add.source().graph());
+        assertFalse(add.silent());
+        assertEquals("<http://example.org/s>", add.source().graph().raw());
         assertTrue(add.destination().defaultGraph());
+    }
+
+    @Test
+    @DisplayName("ADD DEFAULT TO DEFAULT: both source and destination are DEFAULT")
+    void addDefaultToDefault() {
+        AddRequestAst add = assertInstanceOf(AddRequestAst.class,
+                parseUnit("ADD DEFAULT TO DEFAULT"));
+        assertFalse(add.silent());
+        assertTrue(add.source().defaultGraph());
+        assertTrue(add.destination().defaultGraph());
+    }
+
+    @Test
+    @DisplayName("ADD SILENT DEFAULT TO DEFAULT: silent with both DEFAULT")
+    void addSilentDefaultToDefault() {
+        AddRequestAst add = assertInstanceOf(AddRequestAst.class,
+                parseUnit("ADD SILENT DEFAULT TO DEFAULT"));
+        assertTrue(add.silent());
+        assertTrue(add.source().defaultGraph());
+        assertTrue(add.destination().defaultGraph());
+    }
+
+    @Test
+    @DisplayName("chained ADD operations separated by ';' produce two update operations")
+    void chainedAddProducesTwoOperations() {
+        QueryAst ast = newParserDefault().parse(
+                "ADD GRAPH <http://example.org/s> TO GRAPH <http://example.org/d> ; " +
+                "ADD SILENT DEFAULT TO GRAPH <http://example.org/d2>");
+        UpdateRequestAst update = assertInstanceOf(UpdateRequestAst.class, ast);
+        assertEquals(2, update.operations().size());
+
+        AddRequestAst first = assertInstanceOf(AddRequestAst.class, update.operations().get(0));
+        assertEquals("<http://example.org/s>", first.source().graph().raw());
+        assertEquals("<http://example.org/d>", first.destination().graph().raw());
+        assertFalse(first.silent());
+
+        AddRequestAst second = assertInstanceOf(AddRequestAst.class, update.operations().get(1));
+        assertTrue(second.source().defaultGraph());
+        assertEquals("<http://example.org/d2>", second.destination().graph().raw());
+        assertTrue(second.silent());
+    }
+
+    @Test
+    @DisplayName("PREFIX declaration is preserved in the prologue of an ADD query")
+    void addMustKeepItsPrologue() {
+        QueryAst ast = newParserDefault().parse(
+                "PREFIX ex: <http://example.org/> ADD GRAPH ex:s TO GRAPH ex:d");
+        UpdateRequestAst update = assertInstanceOf(UpdateRequestAst.class, ast);
+        assertEquals(1, update.operations().size());
+        assertInstanceOf(AddRequestAst.class, update.operations().getFirst());
+        assertNotNull(update.prologue());
+        assertEquals(1, update.prologue().prefixDeclarations().size());
+    }
+
+    @Test
+    @DisplayName("ADD without TO keyword should throw a syntax exception")
+    void addMissingToShouldFail() {
+        assertThrows(QuerySyntaxException.class,
+                () -> newParserDefault().parse("ADD GRAPH <http://example.org/s>"));
+    }
+
+    @Test
+    @DisplayName("ADD without source should throw a syntax exception")
+    void addMissingSourceShouldFail() {
+        assertThrows(QuerySyntaxException.class,
+                () -> newParserDefault().parse("ADD TO GRAPH <http://example.org/d>"));
     }
 }
